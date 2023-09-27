77b119fa52815096b0a5d0e97ff7b44d217e39b5
oLouis1 <louisovenden1@gmail.com>
2023-09-27 11:44:44 +0100

    fixed mesh with collisons

--- Assets/MeshGen.cs
+++ Assets/MeshGen.cs
@@ -10,8 +10,8 @@ public class MeshGen : MonoBehaviour
     Vector3[] vertices; //a vector for each point along the mesh, describes its x and z location and its height/ y location
     int[] triangles;    //the triangles needed for the mesh
 
-    public int Xsize=20;   //width and length for the mesh
-    public int Zsize=20;
+    public int Xsize=3;   //width and length for the mesh
+    public int Zsize=3;
 
     // Start is called before the first frame update
     void Start()
@@ -25,39 +25,43 @@ public class MeshGen : MonoBehaviour
     }
     void Update()
     {
-        clearMesh();
+       // clearMesh();
     }
 
     void makeMesh()
     {
-        vertices = new Vector3[(Xsize+1) * (Zsize+1)];
-        triangles = new int[((Xsize) * (Zsize) * 6)];
+        vertices = new Vector3[(Xsize) * (Zsize)];
+        triangles = new int[(Xsize-1) * (Zsize-1) * 6];
 
         for (int i = 0,z=0; z < Zsize; z++)     //loops through z and x, creating the vector for each vertex
         {
             for (int x = 0; x < Xsize; x++)
             {
                 float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;  //create the psudo random height for each vertex
-                vertices[i] = new Vector3(x, y, z);
+                vertices[i] = new Vector3(x, 0, z);
+                Debug.Log(z);
                 i++;
             }
         }
-        
-        for (int tris = 0, vertex= 0, z= 0; z < Zsize; z++)
+        int trianglePart = 0, vertex =0;
+        for (int z=0; z<Zsize-1; z++)
         {
-            for (int x = 0; x < Xsize; x++)
+            for (int x = 0; x < Xsize-1; x++)
             {
-                triangles[tris + 0] = vertex + 0;       //creates the triangles looping through x and z again
-                triangles[tris + 1] = vertex + Xsize+1;
-                triangles[tris + 2] = vertex + 1;
-                triangles[tris + 3] = vertex + 1;
-                triangles[tris + 4] = vertex + Xsize + 1;
-                triangles[tris + 5] = vertex + Xsize + 2;
-                tris += 6;
+                triangles[trianglePart + 0] = vertex;           //loops for making triangles for the mesh
+                triangles[trianglePart + 1] = vertex + Xsize+1;
+                triangles[trianglePart + 2] = vertex + 1;
+                triangles[trianglePart + 3] = vertex + 1;
+                triangles[trianglePart + 4] = vertex + Xsize+1;
+                triangles[trianglePart + 5] = vertex + Xsize+2;
                 vertex++;
+                trianglePart += 6;
             }
             vertex++;
         }
+        
+        
+        
     }
 
     void clearMesh()
@@ -68,6 +72,6 @@ public class MeshGen : MonoBehaviour
 
         mesh.RecalculateNormals();
     }
-    // Update is called once per frame
+   
     
 }
